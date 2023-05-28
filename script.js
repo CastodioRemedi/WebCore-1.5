b179d1ed8ecd3533b4a55ce30631c3198ec43415
Nikita Dosmanov <kastodio@gmail.com>
2023-05-28 14:26:18 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+let slider = null;
+function sliderInit() {
+  if (!slider) {
+    slider = new Swiper(".swiper", {
+      direction: "horizontal",
+      spaceBetween: 10,
+      slidesPerView: "auto",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+}
+
+function sliderDestroy() {
+  if (slider) {
+    slider.destroy();
+    slider = null;
+  }
+}
+
+window.addEventListener("resize", function () {
+  if (window.matchMedia("(max-width: 760px)").matches) {
+    sliderInit();
+    console.log("Должен работать");
+  } else {
+    sliderDestroy();
+    console.log("Должен не работать");
+  }
+});
+
+const slides = document.querySelectorAll(".swiper-slide");
+const btn = document.querySelector(".expand");
+const mediaQuery = window.matchMedia("(min-width: 976px)");
+
+function buttonChange() {
+  if (btn.innerHTML == "Показать всё") {
+    btn.innerHTML = "Скрыть";
+    btn.classList.add("expand--down");
+    btn.classList.remove("expand--on");
+  } else {
+    btn.innerHTML = "Показать всё";
+    btn.classList.add("expand--on");
+    btn.classList.remove("expand--down");
+  }
+}
+
+function showSlides() {
+  if (btn.innerHTML == "Показать всё") {
+    slides.forEach((item) => {
+      item.style.display = "none" ? (item.style.display = "flex") : "";
+    });
+  }
+}
+
+function closeSlidesMiddle() {
+  for (let i = 6; i < slides.length; i++) {
+    slides[i].style.display = "flex" ? (slides[i].style.display = "none") : "";
+  }
+  buttonChange();
+}
+
+function closeSlidesHuge() {
+  for (let i = 8; i < slides.length; i++) {
+    slides[i].style.display = "flex" ? (slides[i].style.display = "none") : "";
+  }
+  buttonChange();
+}
+
+function toggleSlideDisplay() {
+  if (btn.innerHTML == "Показать всё") {
+    slides.forEach((item) => {
+      item.style.display = "none" ? (item.style.display = "flex") : "";
+    });
+    buttonChange();
+  } else {
+    mediaQuery.matches ? closeSlidesHuge() : closeSlidesMiddle();
+  }
+}
+
+btn.addEventListener("click", toggleSlideDisplay);
